9e5cf5e66965d485a49a99710b0e5619faaa2b76
IgorFMouta <89466547+IgorFMouta@users.noreply.github.com>
2021-11-17 15:33:57 -0300

    Criação da classe pedido

---
+++ src/main/java/org/serratec/backend/apiRestfulG5/domain/Pedido.java
@@ -0,0 +1,58 @@
+package org.serratec.backend.apiRestfulG5.domain;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import org.serratec.backend.apiRestfulG5.enums.PedidoStatus;
+
+//import br.org.serratec.backend.ecommerce.enums.PedidoStatus;
+//import lombok.Data;
+
+@Entity
+@Table(name = "pedido")
+// @Data
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "data_pedido")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataPedido;
+
+	@Column(name="status", nullable = false)
+	private PedidoStatus pedidoStatus;
+
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
+
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<Item_pedido> itens = new HashSet<>();
+
+
+	@Transient
+	public Double getTotal() {
+		double soma = 0.0;
+		for(Item_pedido item : itens) {
+			soma += item.getSubTotal();
+		}
+		return soma;
+	}
+
+	
+	
+}

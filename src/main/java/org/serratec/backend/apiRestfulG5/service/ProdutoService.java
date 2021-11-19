bd99f7d4230c11d8f69b74018f7658784171a52b
Kelvin Penha <89466425+KelvinKSP@users.noreply.github.com>
2021-11-19 12:06:37 -0300

    Produto Service - Cliente Exception

--- src/main/java/org/serratec/backend/apiRestfulG5/service/ProdutoService.java
+++ src/main/java/org/serratec/backend/apiRestfulG5/service/ProdutoService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProdutoService {
-	
+
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
@@ -21,15 +21,62 @@ public class ProdutoService {
 		return produtoRepository.findAll();
 	}
 	
+	
+	public Produto listarPorId(Long id_produto) throws ProdutoNotFoundException {
+		Optional<Produto> opProduto = produtoRepository.findById(id_produto);
+		
+		if(opProduto.isPresent()) {
+			return opProduto.get();
+		}
+		
+		throw new ProdutoNotFoundException("O cliente com a id " + id_produto + " não foi encontrado");
+		
+	}
+		
 	public Produto inserir(Produto produto) {
 		return produtoRepository.save(produto);
 	}
-	
-	
 
+	
+	public Produto atualizar(Long id_produto, Produto produto) throws ParametroObrigatorioException, ProdutoNotFoundException{
+		if(produto == null) throw new ParametroObrigatorioException("Campo 'Produto' é obrigatório");
+	
+		Produto produtoBanco = listarPorId(id_produto);
 		
+		if(produto.getNome() != null) {
+			produtoBanco.setNome(produto.getNome());
+		}
 		
+		if(produto.getDescricao() != null) {
+			produtoBanco.setDescricao(produto.getDescricao());
+		}
 		
+		if(produto.getQtd_estoque() != null) {
+			produtoBanco.setQtd_estoque(produto.getQtd_estoque());
+		}
+		
+		if(produto.getData_cadastro() != null) {
+			produtoBanco.setData_cadastro(produto.getData_cadastro());
+		}
+		
+		if(produto.getValor_unitario() != null) {
+			produtoBanco.setValor_unitario(produto.getValor_unitario());
+		}
+		
+		if(produto.getImage() != null) {
+			produtoBanco.setImage(produto.getImage());
+		}
+		
+		return produtoRepository.save(produtoBanco);		
+	}
+		
+	
+	public void deletarPorId(Long id_produto) throws ProdutoNotFoundException{
+		Produto produtoBanco = listarPorId(id_produto);
+		produtoRepository.delete(produtoBanco);
+	}
+	
+	
 		
 
 
